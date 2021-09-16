bda81d72c07eb98c8ed66d35fa42c600529a2bf9
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-09-16 17:45:15 +0200

    pass RG

--- coreservices/partsrelationshipservice/terraform/variables.tf
+++ coreservices/partsrelationshipservice/terraform/variables.tf
@@ -12,20 +12,6 @@ variable "environment" {
   default = "dev"
 }
 
-variable "location" {
-  type    = string
-  default = "germanywestcentral"
-}
-
-variable "kubernetes_host" {
-  type = string
-}
-variable "kubernetes_client_key_base64" {
-  type = string
-}
-variable "kubernetes_client_certificate_base64" {
-  type = string
-}
-variable "kubernetes_cluster_ca_certificate_base64" {
+variable "resource_group_name" {
   type = string
 }
--- coreservices/partsrelationshipservice/terraform/versions.tf
+++ coreservices/partsrelationshipservice/terraform/versions.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "2.60.0"
     }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.1.2"
-    }
   }
 
   # Persist state in a storage account
@@ -26,13 +21,11 @@ provider "azurerm" {
   features {}
 }
 
-provider "helm" {
-  debug = true
-  kubernetes {
-    host                   = var.kubernetes_host
-    client_key             = base64decode(var.kubernetes_client_key_base64)
-    client_certificate     = base64decode(var.kubernetes_client_certificate_base64)
-    cluster_ca_certificate = base64decode(var.kubernetes_cluster_ca_certificate_base64)
-  }
+data "azurerm_resource_group" "main" {
+  name                = var.resource_group_name
 }
 
+locals {
+  resource_group_name = data.azurerm_resource_group.main.name
+  location        = data.azurerm_resource_group.main.location
+}
