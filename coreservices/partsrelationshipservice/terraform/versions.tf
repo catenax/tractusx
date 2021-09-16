11cd2119f0f59d38379d35606740a5006003a339
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-09-16 18:07:49 +0200

    Update versions.tf

--- coreservices/partsrelationshipservice/terraform/versions.tf
+++ coreservices/partsrelationshipservice/terraform/versions.tf
@@ -40,6 +40,13 @@ locals {
   location        = data.azurerm_resource_group.main.location
 }
 
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.main.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
+}
+
 provider "helm" {
   debug = true
   kubernetes {
