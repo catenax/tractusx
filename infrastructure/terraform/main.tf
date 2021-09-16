370884a426ed5e1800043b9aceedfd0cb86f2b15
Ophelie Le Mentec <olementec@microsoft.com>
2021-09-16 13:15:09 +0200

    Added app insight, eventhub and postgres.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -257,3 +257,37 @@ resource "kubernetes_namespace" "portal_namespace" {
     name = "portal"
   }
 }
+
+####################################################################################################
+# PRS infrastructure
+####################################################################################################
+
+module "prs_application_insights" {
+  source = "./modules/application-insights"
+
+  name                = "${var.prefix}-${var.environment}-prs-appi"
+  resource_group_name = azurerm_resource_group.default_rg.name
+  location            = azurerm_resource_group.default_rg.location
+}
+
+module "prs_postgresql" {
+  source              = "./modules/postgresql"
+  name                = "${var.prefix}-${var.environment}-prs-psql"
+  database_name       = "prs"
+  resource_group_name = azurerm_resource_group.default_rg.name
+  location            = azurerm_resource_group.default_rg.location
+}
+
+module "eventhubs_namespace" {
+  source              = "./modules/eventhubs_namespace"
+  name                = "${var.prefix}-${var.environment}-prs-ehub"
+  resource_group_name = azurerm_resource_group.default_rg.name
+  location            = azurerm_resource_group.default_rg.location
+}
+
+module "eventhub_catenax_events" {
+  source                  = "./modules/eventhub"
+  eventhub_namespace_name = module.eventhubs_namespace.name
+  name                    = "catenax_events"
+  resource_group_name     = azurerm_resource_group.default_rg.name
+}
