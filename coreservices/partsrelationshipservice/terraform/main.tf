390e056676210b3babde25497bb3254bd18539c5
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-09-16 18:18:00 +0200

    Update main.tf

--- coreservices/partsrelationshipservice/terraform/main.tf
+++ coreservices/partsrelationshipservice/terraform/main.tf
@@ -42,7 +42,7 @@ resource "kubernetes_namespace" "prs" {
 # deploy NGINX chart with Helm
 resource "helm_release" "dummy" {
   name       = "dummy"
-  chart      = "bitnami/rabbitmq"
+  chart      = "rabbitmq"
   namespace  = kubernetes_namespace.prs.metadata[0].name
   repository = "https://charts.bitnami.com/bitnami"
   timeout    = 300
