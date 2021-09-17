fa77eb16272cb1ec8676fa97b94e01bbd5f7b8e8
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-09-17 17:21:03 +0200

    Update PrsIntegrationTests.java

--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/test/PrsIntegrationTests.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/test/PrsIntegrationTests.java
@@ -1,9 +1,14 @@
 package net.catenax.prs.test;
 
 import io.micrometer.core.instrument.util.IOUtils;
+import io.restassured.RestAssured;
 import net.catenax.prs.PrsApplication;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static io.restassured.RestAssured.*;
@@ -13,9 +18,17 @@ import static org.hamcrest.Matchers.*;
 @SpringBootTest(classes = {PrsApplication.class}, webEnvironment = RANDOM_PORT)
 public class PrsIntegrationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUpClass(){
+        RestAssured.port = port;
+    }
+
     @Test
     public void greetingShouldReturnDefaultMessage() {
-        get("/api/v0.1/vins/BMWOVCDI21L5DYEUU/partsTree").then().assertThat().body(equalTo(
+        get("/api/v0.1/vins/BMWOVCDI21L5DYEUU/partsTree?view=AS_MAINTAINED").then().assertThat().body(equalTo(
         IOUtils.toString( getClass().getClassLoader().getResourceAsStream("response_1631610272167.json"))));
     }
 }
