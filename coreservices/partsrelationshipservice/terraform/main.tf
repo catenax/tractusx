6bbeeffe85188059aff3a88f26a8bb27e7d15e48
Ophelie Le Mentec <olementec@microsoft.com>
2021-09-17 09:09:49 +0200

    Revert "test2."
    This reverts commit 3d5260a66959c76091360f1c41c6f5eb7031d26f.

--- coreservices/partsrelationshipservice/terraform/main.tf
+++ coreservices/partsrelationshipservice/terraform/main.tf
@@ -64,7 +64,6 @@ resource "helm_release" "prs" {
   set {
     name = "applicationInsights.connectionString"
     value = module.prs_application_insights.connection_string
-    sensitive = true
   }
 
   set {
@@ -80,13 +79,11 @@ resource "helm_release" "prs" {
   set {
     name = "eventHubs.sendConnectionString"
     value = module.eventhub_catenax_events.send_primary_connection_string
-    sensitive = true
   }
 
   set {
     name = "eventHubs.listenConnectionString"
     value = module.eventhub_catenax_events.listen_primary_connection_string
-    sensitive = true
   }
 
   set {
@@ -102,7 +99,6 @@ resource "helm_release" "prs" {
   set {
     name = "postgresql.postgresqlPassword"
     value = module.prs_postgresql.administrator_login_password
-    sensitive = true
   }
 
 }
