11bbd7df59bb93f236bef1ca669cd8818c75c30a
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-20 13:49:40 +0200

    https://jira.catena-x.net/browse/CATART0PO6-81 This was left from review.

--- portal/code/tractus-x-portal/src/components/aspect.tsx
+++ portal/code/tractus-x-portal/src/components/aspect.tsx
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { ThemeGenerator } from '@fluentui/react';
 import * as React from 'react';
 import { isJsxOpeningElement } from 'typescript';
 import BackLink from './navigation/backlink';
