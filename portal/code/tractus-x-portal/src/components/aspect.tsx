f90f0e5a43870ff02a54f7626689d41d9696971a
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-20 13:48:45 +0200

    https://jira.catena-x.net/browse/CATART0PO6-81 Incorporate results of review.

--- portal/code/tractus-x-portal/src/components/aspect.tsx
+++ portal/code/tractus-x-portal/src/components/aspect.tsx
@@ -15,33 +15,33 @@
 import { ThemeGenerator } from '@fluentui/react';
 import * as React from 'react';
 import { isJsxOpeningElement } from 'typescript';
-import BackLink from "./navigation/backlink";
+import BackLink from './navigation/backlink';
 import { Dropdown, IDropdownOption, IDropdownStyles, SearchBox } from '@fluentui/react';
 
 export default class Aspect extends React.Component<any, any> {
 
   headers = new Headers(
-    {"Content-Type":"application/json",
-     "Authorization":"Basic "+process.env.REACT_APP_CONNECTOR_AUTHENTICATION
+    {'Content-Type':'application/json',
+     'Authorization':`Basic ${process.env.REACT_APP_CONNECTOR_AUTHENTICATION}`
     }
   );
 
   mounted = false
-  catalog = "catenax-catalog"
+  catalog = 'catenax-catalog'
 
   constructor(props) {
     super(props);
 
     let params=props.match.params;
 
-    this.state = { params:props.match.params, value: 'App Connector Session '+JSON.stringify(params)};
+    this.state = { params:props.match.params, value: `App Connector Session ${JSON.stringify(params)}`;
       
     this.handleValueChange = this.handleValueChange.bind(this);
     this.onOfferChange = this.onOfferChange.bind(this);
     this.onRepresentationChange = this.onRepresentationChange.bind(this);
     this.onArtifactChange = this.onArtifactChange.bind(this);
 
-    if(this.state.params.offer!=undefined && this.state.params.offer!=undefined && this.state.params.artifact!=undefined) {
+    if(this.state.params.offer!=undefined && this.state.params.representation!=undefined && this.state.params.artifact!=undefined) {
       this.findCatalog();
     } 
   }
@@ -74,7 +74,7 @@ export default class Aspect extends React.Component<any, any> {
     let params=this.state.params;
     params['artifact']=option.key;
     this.setState({params:params, value: this.state.value});
-    if(this.state.params.offer!=undefined && this.state.params.offer!=undefined && this.state.params.artifact!=undefined) {
+    if(this.state.params.offer!=undefined && this.state.params.representation!=undefined && this.state.params.artifact!=undefined) {
       this.findCatalog();
     } 
   }
@@ -100,17 +100,17 @@ export default class Aspect extends React.Component<any, any> {
       //, body: raw
     };
 
-    that.appendOutput(">>>GET "+url);
+    that.appendOutput(`>>>GET ${url}`);
 
     fetch(url, requestOptions)
     .then( answer => answer.text())
     .then( text => {
       if(!pp) {
-        that.appendOutput("<<<GET "+text);
+        that.appendOutput(`<<<GET ${text}`);
       }
       let result=JSON.parse(text);
       if(pp) {
-        that.appendOutput("<<<GET "+JSON.stringify(result,null,2));
+        that.appendOutput(`<<<GET ${JSON.stringify(result,null,2)}`);
       } 
       setTimeout(function (){
 
@@ -134,136 +134,143 @@ export default class Aspect extends React.Component<any, any> {
       body: text
     };
 
-    that.appendOutput(">>>POST "+url+ " "+text);
+    that.appendOutput(`>>>POST ${url} ${text}`);
 
     fetch(url, requestOptions)
     .then( answer => answer.text())
     .then( text => {
-      that.appendOutput("<<<POST "+text); 
+      that.appendOutput(`<<<POST ${text}`); 
       let result=JSON.parse(text);
       continuation(result);
     })
     .catch( error => console.log(error.toString()));
   }
 
+  /** find catena-x catalog (and trigger find offering afterwards) */
   findCatalog() {
     const that = this;
 
-    that.performGet(process.env.REACT_APP_BROKER_ENDPOINT+"/api/catalogs", function(catalogs) {
+    that.performGet(`${process.env.REACT_APP_BROKER_ENDPOINT}/api/catalogs`, function(catalogs) {
       let catalogues=catalogs._embedded.catalogs;
       for(let catalog of catalogues) {
         if(catalog.title === that.catalog) {
           let fullId=catalog._links.self.href;
-          that.appendOutput("$$$CATALOG found "+that.catalog+" under id "+fullId);
-          that.appendOutput("");
+          that.appendOutput(`$$$CATALOG found ${that.catalog} under id ${fullId}`);
+          that.appendOutput('');
           return that.findOffer(fullId);
         } 
       }
-      that.appendOutput("!!!CATALOG "+that.catalog+" was not found.");        
+      that.appendOutput(`!!!CATALOG ${that.catalog} was not found.`);        
     });
   }
 
+  /** find the selected offer (and trigger finding the representation, afterwards) */
   findOffer(catalogUrl) {
     const that = this;
 
-    that.performGet(catalogUrl+"/offers", function(offers) {
+    that.performGet(`${catalogUrl}/offers`, function(offers) {
       let offerings=offers._embedded.resources;
       for(let offer of offerings) {
         if(offer.title === that.state.params.offer) {
           let fullId=offer._links.self.href;
-          that.appendOutput("$$$OFFER found "+that.state.params.offer+" under id "+fullId);
-          that.appendOutput("");
+          that.appendOutput(`$$$OFFER found ${that.state.params.offer} under id ${fullId}`);
+          that.appendOutput('');
           let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
-          return that.findRepresentation(shortId,fullId);
+          return that.findRepresentation(fullId);
         } 
       }
-      that.appendOutput("$$$OFFER "+that.state.params.offer+" was not found.");        
+      that.appendOutput(`$$$OFFER ${that.state.params.offer} was not found.`);        
     });
   }
 
-  findRepresentation(offerId,offerUrl) {
+  /** find the selected representation (and trigger finding the artifact, afterwards) */
+  findRepresentation(offerUrl) {
     const that = this;
 
-    that.performGet(offerUrl+"/representations", function(reps) {
+    that.performGet(`${offerUrl}/representations`, function(reps) {
       let representations=reps._embedded.representations;
       for(let rep of representations) {
         if(rep.title === that.state.params.representation) {
           let fullId=rep._links.self.href;
-          that.appendOutput("$$$REPRESENTATION "+that.state.params.representation+" under id "+fullId);
-          that.appendOutput("");
+          that.appendOutput(`$$$REPRESENTATION ${that.state.params.representation} under id ${fullId}`);
+          that.appendOutput('');
           let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
           return that.findArtifact(offerUrl,shortId,fullId);
         } 
       }
-      that.appendOutput("!!!REPRESENTATION "+that.state.params.representation+" was not found.");        
+      that.appendOutput(`!!!REPRESENTATION ${that.state.params.representation} was not found.`);        
     });
   }
 
+  /** find the selected artifact (and trigger creation of an agreement, afterwards) */
   findArtifact(offerId,repId,repUrl) {
     const that = this;
 
-    that.performGet(repUrl+"/artifacts", function(arts) {
+    that.performGet(`${repUrl}/artifacts`, function(arts) {
       let artifacts=arts._embedded.artifacts;
       for(let art of artifacts) {
         if(art.title === that.state.params.artifact) {
           let fullId=art._links.self.href;
-          that.appendOutput("$$$ARTIFACT found "+that.state.params.artifact+" under id "+fullId);
-          that.appendOutput("");
+          that.appendOutput(`$$$ARTIFACT found ${that.state.params.artifact} under id ${fullId}`);
+          that.appendOutput('');
           let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
-          return that.agreement(offerId,repId,shortId,fullId);
+          return that.agreement(offerId,fullId);
         } 
       }
-      that.appendOutput("!!!ARTIFACT "+that.state.params.artifact+" was not found.");        
+      that.appendOutput(`!!!ARTIFACT ${that.state.params.artifact} was not found.`);        
     });
   }
 
-  agreement(offerId,repId,artifactId,artifactFullId) {
+  /** create an agreement (and trigger finding the resulting local artifact, afterwards) */
+  agreement(offerId,artifactFullId) {
 
     const that = this;
     
     const raw = [
     {
-      "@type": "ids:Permission",
-      "ids:action": [],
-      "ids:target": artifactFullId
+      '@type': 'ids:Permission',
+      'ids:action': [],
+      'ids:target': artifactFullId
     }];
 
-    that.performPost(process.env.REACT_APP_CONNECTOR_ENDPOINT+"/api/ids/contract?recipient=http://localhost:8080/api/ids/data&resourceIds="+offerId+"&artifactIds="+artifactFullId+"&download=false", 
+    that.performPost(`${process.env.REACT_APP_CONNECTOR_ENDPOINT}/api/ids/contract?recipient=${process.env.REACT_APP_BROKER_ENDPOINT}/api/ids/data&resourceIds=${offerId}&artifactIds=${artifactFullId}&download=false`, 
       raw, function(agreement) {
         var remoteAgreement = agreement.remoteId
         var fullId=agreement._links.self.href
         var shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
-        that.appendOutput("$$$AGREEMENT negotiated "+fullId+ "with remote "+remoteAgreement);
-        that.appendOutput("");
-        that.findLocalArtifact(remoteAgreement,shortId,fullId);
-        // that.appendOutput("! Artifact "+that.artifact+" was not found.");        
+        that.appendOutput(`$$$AGREEMENT negotiated ${fullId} with remote ${remoteAgreement}`);
+        that.appendOutput('');
+        that.findLocalArtifact(remoteAgreement,fullId);
+        // that.appendOutput('! Artifact '+that.artifact+' was not found.');        
     });
   }
 
-  findLocalArtifact(remoteAgreement,agreementId,agreementUrl) {
+  /** find an agreed artifact (and trigger downloading, afterwards) */
+  findLocalArtifact(remoteAgreement,agreementUrl) {
     const that = this;
 
-    that.performGet(agreementUrl+"/artifacts", function(arts) {
+    that.performGet(`${agreementUrl}/artifacts`, function(arts) {
       let artifacts=arts._embedded.artifacts;
       for(let art of artifacts) {
         let fullId=art._links.self.href;
-        that.appendOutput("$$$ARTIFACT negotiated registered under id "+fullId);
-        that.appendOutput("");
+        that.appendOutput(`$$$ARTIFACT negotiated registered under id ${fullId}`);
+        that.appendOutput('');
         let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
         that.download(fullId,remoteAgreement);
       }
     });
   }
 
+  /** download an agree artifact */
   download(artifactUrl,remoteAgreement) {
     const that = this;
-    that.performGet(artifactUrl+"/data/**?download=true&agreementUri="+remoteAgreement, function(body) {
+    that.performGet(`${artifactUrl}/data/**?download=true&agreementUri=${remoteAgreement}`, function(body) {
       let text = JSON.stringify(body);
-      that.appendOutput("^^^ Got Result with "+text.length+" bytes.");
+      that.appendOutput(`^^^ Got Result with ${text.length} bytes.`);
     },true)
   }
 
-
+  /** render the debugging view */
   render() {
     let backlink;
     if(this.props.history !== undefined) {
@@ -288,31 +295,31 @@ export default class Aspect extends React.Component<any, any> {
     let artifact=this.state.params.artifact
     return(
       <div className='h100pc df fdc p44'>
-        <div className="df jcsb w100pc">
+        <div className='df jcsb w100pc'>
           {backlink}
-          <Dropdown placeholder="Filter"
-            label="Offer"
+          <Dropdown placeholder='Filter'
+            label='Offer'
             options={availableOffers}
             defaultSelectedKey={offer}
             styles={dropdownStyles}
             onChange={this.onOfferChange}
           />
-          <Dropdown placeholder="Filter"
-            label="Representation"
+          <Dropdown placeholder='Filter'
+            label='Representation'
             options={availableRepresentations}
             defaultSelectedKey={representation}
             styles={dropdownStyles}
             onChange={this.onRepresentationChange}
           />
-          <Dropdown placeholder="Filter"
-            label="Artifact"
+          <Dropdown placeholder='Filter'
+            label='Artifact'
             options={availableArtifacts}
             defaultSelectedKey={artifact}
             styles={dropdownStyles}
             onChange={this.onArtifactChange}
           />
         </div>
-        <div className="p4 fg1 bgindustrial fgf2 fs12"><pre>{this.state.value}</pre></div>
+        <div className='p4 fg1 bgindustrial fgf2 fs12'><pre>{this.state.value}</pre></div>
       </div>
     );
   }
