985366eba76d30358360220b014bd61fc7a8bff8
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-21 08:25:28 +0200

    https://jira.catena-x.net/browse/CATART0PO6-5 Use an init step to determine target workspace. Enable terraform to work with service principal.

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -17,3 +17,23 @@ variable "environment" {
   description = "Environment: dev<nnn>, int or prod"
   default = "dev001"
 }
+
+variable "azure_subscription_id" {
+  description = "Subscription ID of the deployment principal"
+  type = string
+}
+
+variable "azure_client_id" {
+  description = "Client ID of the deployment principal"
+  type = string
+}
+
+variable "azure_client_secret" {
+  description = "Client Secret of the deployment principal"
+  type = string
+}
+
+variable "azure_tenant_id" {
+  description = "Tenant ID of the deployment principal"
+  type = string
+}
