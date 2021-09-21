6d3d3761055be81ea0b4024016fbcd6b7cc7f540
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-21 13:46:11 +0200

    https://jira.catena-x.net/browse/CATART0PO6-5 redundant parenthesis.

--- infrastructure/terraform/versions.tf
+++ infrastructure/terraform/versions.tf
@@ -25,8 +25,8 @@ terraform {
     resource_group_name  = "terraform-rg"
     storage_account_name = "catenaxdevtfstate"
     container_name       = "tfstate"
-    key                  = "catenaxdev.tfstateenv:${{var.environment}"
-    access_key           = "${{var.azure_storage_access_key}}"
+    key                  = "catenaxdev.tfstateenv:${var.environment}"
+    access_key           = var.azure_storage_access_key
   }
 
   required_version = "~> 0.14"
