d9529cf6b7c2ffb20c3c135c8a8538f9fd15d7c7
JaneMcBrain <contact@janemcbrain.com>
2021-09-23 14:35:23 +0200

    cleanup aspect.tsx https://jira.catena-x.net/browse/CATART0PO6-35

--- portal/code/tractus-x-portal/src/components/digitaltwins/aspect.tsx
+++ portal/code/tractus-x-portal/src/components/digitaltwins/aspect.tsx
@@ -30,18 +30,27 @@ export default class Aspect extends React.Component<any, any> {
   constructor(props) {
     super(props);
 
-    let params=props.match.params;
+    let params = props.match.params;
 
-    this.state = { params:props.match.params, value: `App Connector Session ${JSON.stringify(params)}`};
+    this.state = {
+      params: params,
+      value: `App Connector Session ${JSON.stringify(params)}`
+    };
       
     this.handleValueChange = this.handleValueChange.bind(this);
     this.onOfferChange = this.onOfferChange.bind(this);
     this.onRepresentationChange = this.onRepresentationChange.bind(this);
     this.onArtifactChange = this.onArtifactChange.bind(this);
 
-    if(this.state.params.offer!=undefined && this.state.params.representation!=undefined && this.state.params.artifact!=undefined) {
+    if(this.paramsUndefined()) {
       this.findCatalog();
     } 
+  }
+
+  paramsUndefined(){
+    return this.state.params.offer != undefined && 
+      this.state.params.representation != undefined &&
+      this.state.params.artifact != undefined;
   }
 
   componentDidMount() { 
@@ -49,48 +58,47 @@ export default class Aspect extends React.Component<any, any> {
   }
   
   componentWillUnmount() {
-     this.mounted = false;
+    this.mounted = false;
   }
 
   handleValueChange(event) {
-    this.setState({params:this.state.params, value: event.target.value});
+    this.setState({params: this.state.params, value: event.target.value});
   }
 
-  onOfferChange(event,option) {
-    let params=this.state.params;
-    params['offer']=option.key;
-    this.setState({params:params, value: this.state.value});
+  onOfferChange(event, option) {
+    let params = this.state.params;
+    params['offer'] = option.key;
+    this.setState({params: params});
   }
 
-  onRepresentationChange(event,option) {
-    let params=this.state.params;
-    params['representation']=option.key;
-    this.setState({params:params, value: this.state.value});
+  onRepresentationChange(event, option) {
+    let params = this.state.params;
+    params['representation'] = option.key;
+    this.setState({params: params});
   }
 
-  onArtifactChange(event,option) {
-    let params=this.state.params;
-    params['artifact']=option.key;
-    this.setState({params:params, value: this.state.value});
-    if(this.state.params.offer!=undefined && this.state.params.representation!=undefined && this.state.params.artifact!=undefined) {
+  onArtifactChange(event, option) {
+    let params = this.state.params;
+    params['artifact'] = option.key;
+    this.setState({params: params});
+    if(this.paramsUndefined()) {
       this.findCatalog();
-    } 
+    }
   }
 
   /** console output */
   appendOutput(text) {
     console.log(text);
     if(this.mounted) {
-      this.setState({params:this.state.params,value: `${text}\n${this.state.value}`});
+      this.setState({value: `${text}\n${this.state.value}`});
     } else {
       this.state = {params:this.state.params, value: `${text}\n${this.state.value}`};
     }
   }
 
   /** performs basic get and returns an any promise*/
-  performGet(url,continuation,pp=false) {
-    
-    const that=this;
+  performGet(url, continuation, pp = false) {
+    const that = this;
 
     let requestOptions = {
       method: 'GET',
@@ -101,26 +109,26 @@ export default class Aspect extends React.Component<any, any> {
     that.appendOutput(`>>>GET ${url}`);
 
     fetch(url, requestOptions)
-    .then( answer => answer.text())
-    .then( text => {
-      if(!pp) {
-        that.appendOutput(`<<<GET ${text}`);
-      }
-      let result=JSON.parse(text);
-      if(pp) {
-        that.appendOutput(`<<<GET ${JSON.stringify(result,null,2)}`);
-      } 
-      setTimeout(function (){
+      .then( answer => answer.text())
+      .then( text => {
+        if(!pp) {
+          that.appendOutput(`<<<GET ${text}`);
+        }
+        let result = JSON.parse(text);
+        if(pp) {
+          that.appendOutput(`<<<GET ${JSON.stringify(result, null, 2)}`);
+        } 
+        setTimeout(function (){
 
-        continuation(result);
-      
-      }, 1000);
-    })
-    .catch( error => console.log(error.toString()));
+          continuation(result);
+        
+        }, 1000);
+      })
+      .catch( error => console.log(error.toString()));
   }
 
   /** performs basic get and returns an any promise*/
-  performPost(url,bdy,continuation) {
+  performPost(url, bdy, continuation) {
     
     const that=this;
 
@@ -135,13 +143,13 @@ export default class Aspect extends React.Component<any, any> {
     that.appendOutput(`>>>POST ${url} ${text}`);
 
     fetch(url, requestOptions)
-    .then( answer => answer.text())
-    .then( text => {
-      that.appendOutput(`<<<POST ${text}`); 
-      let result=JSON.parse(text);
-      continuation(result);
-    })
-    .catch( error => console.log(error.toString()));
+      .then( answer => answer.text())
+      .then( text => {
+        that.appendOutput(`<<<POST ${text}`); 
+        let result = JSON.parse(text);
+        continuation(result);
+      })
+      .catch( error => console.log(error.toString()));
   }
 
   /** find catena-x catalog (and trigger find offering afterwards) */
@@ -170,10 +178,9 @@ export default class Aspect extends React.Component<any, any> {
       let offerings=offers._embedded.resources;
       for(let offer of offerings) {
         if(offer.title === that.state.params.offer) {
-          let fullId=offer._links.self.href;
+          let fullId = offer._links.self.href;
           that.appendOutput(`$$$OFFER found ${that.state.params.offer} under id ${fullId}`);
           that.appendOutput('');
-          let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
           return that.findRepresentation(fullId);
         } 
       }
@@ -186,14 +193,13 @@ export default class Aspect extends React.Component<any, any> {
     const that = this;
 
     that.performGet(`${offerUrl}/representations`, function(reps) {
-      let representations=reps._embedded.representations;
+      let representations = reps._embedded.representations;
       for(let rep of representations) {
         if(rep.title === that.state.params.representation) {
-          let fullId=rep._links.self.href;
+          let fullId = rep._links.self.href;
           that.appendOutput(`$$$REPRESENTATION ${that.state.params.representation} under id ${fullId}`);
           that.appendOutput('');
-          let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
-          return that.findArtifact(offerUrl,shortId,fullId);
+          return that.findArtifact(offerUrl, fullId);
         } 
       }
       that.appendOutput(`!!!REPRESENTATION ${that.state.params.representation} was not found.`);        
@@ -201,18 +207,17 @@ export default class Aspect extends React.Component<any, any> {
   }
 
   /** find the selected artifact (and trigger creation of an agreement, afterwards) */
-  findArtifact(offerId,repId,repUrl) {
+  findArtifact(offerId, repUrl) {
     const that = this;
 
     that.performGet(`${repUrl}/artifacts`, function(arts) {
-      let artifacts=arts._embedded.artifacts;
+      let artifacts = arts._embedded.artifacts;
       for(let art of artifacts) {
         if(art.title === that.state.params.artifact) {
-          let fullId=art._links.self.href;
+          let fullId = art._links.self.href;
           that.appendOutput(`$$$ARTIFACT found ${that.state.params.artifact} under id ${fullId}`);
           that.appendOutput('');
-          let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
-          return that.agreement(offerId,fullId);
+          return that.agreement(offerId, fullId);
         } 
       }
       that.appendOutput(`!!!ARTIFACT ${that.state.params.artifact} was not found.`);        
@@ -220,7 +225,7 @@ export default class Aspect extends React.Component<any, any> {
   }
 
   /** create an agreement (and trigger finding the resulting local artifact, afterwards) */
-  agreement(offerId,artifactFullId) {
+  agreement(offerId, artifactFullId) {
 
     const that = this;
     
@@ -233,47 +238,41 @@ export default class Aspect extends React.Component<any, any> {
 
     that.performPost(`${process.env.REACT_APP_CONNECTOR_ENDPOINT}/api/ids/contract?recipient=${process.env.REACT_APP_BROKER_ENDPOINT}/api/ids/data&resourceIds=${offerId}&artifactIds=${artifactFullId}&download=false`, 
       raw, function(agreement) {
-        var remoteAgreement = agreement.remoteId
-        var fullId=agreement._links.self.href
-        var shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
+        var remoteAgreement = agreement.remoteId;
+        var fullId = agreement._links.self.href;
         that.appendOutput(`$$$AGREEMENT negotiated ${fullId} with remote ${remoteAgreement}`);
         that.appendOutput('');
-        that.findLocalArtifact(remoteAgreement,fullId);
+        that.findLocalArtifact(remoteAgreement, fullId);
         // that.appendOutput('! Artifact '+that.artifact+' was not found.');        
     });
   }
 
   /** find an agreed artifact (and trigger downloading, afterwards) */
-  findLocalArtifact(remoteAgreement,agreementUrl) {
+  findLocalArtifact(remoteAgreement, agreementUrl) {
     const that = this;
 
     that.performGet(`${agreementUrl}/artifacts`, function(arts) {
       let artifacts=arts._embedded.artifacts;
       for(let art of artifacts) {
-        let fullId=art._links.self.href;
+        let fullId = art._links.self.href;
         that.appendOutput(`$$$ARTIFACT negotiated registered under id ${fullId}`);
         that.appendOutput('');
-        let shortId=fullId.substring(fullId.lastIndexOf('/') + 1)
-        that.download(fullId,remoteAgreement);
+        that.download(fullId, remoteAgreement);
       }
     });
   }
 
   /** download an agree artifact */
-  download(artifactUrl,remoteAgreement) {
+  download(artifactUrl, remoteAgreement) {
     const that = this;
     that.performGet(`${artifactUrl}/data/**?download=true&agreementUri=${remoteAgreement}`, function(body) {
       let text = JSON.stringify(body);
       that.appendOutput(`^^^ Got Result with ${text.length} bytes.`);
-    },true)
+    }, true)
   }
 
   /** render the debugging view */
   render() {
-    let backlink;
-    if(this.props.history !== undefined) {
-      backlink=<BackLink history={this.props.history} />;
-    }
     const dropdownStyles: Partial<IDropdownStyles> = {
       dropdown: { width: 400, marginRight: 20 },
     };
@@ -288,13 +287,13 @@ export default class Aspect extends React.Component<any, any> {
       { key: 'material-brake', text: 'Sample Data Source (here: a file) using a transformation.' },
       { key: 'bom-brake', text: 'Sample Data Source (here: a file) using a second transformation.' }
     ];
-    let offer=this.state.params.offer
-    let representation=this.state.params.representation
-    let artifact=this.state.params.artifact
+    let offer = this.state.params.offer;
+    let representation = this.state.params.representation;
+    let artifact = this.state.params.artifact;
     return(
       <div className='h100pc df fdc p44'>
-        <div className='df jcsb w100pc'>
-          {backlink}
+        {this.props.history && <BackLink history={this.props.history} />}
+        <div className='df fdc aife mb20 w100pc'>
           <Dropdown placeholder='Filter'
             label='Offer'
             options={availableOffers}
