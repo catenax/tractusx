1323329dac76503124ab76ce4839d8f230f5f624
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2021-09-24 16:37:30 +0200

    Adding more integration test for aspect data

--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/test/GetPartsTreeByObjectIdIntegrationTests.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/test/GetPartsTreeByObjectIdIntegrationTests.java
@@ -32,6 +32,7 @@ public class GetPartsTreeByObjectIdIntegrationTests extends PrsIntegrationTestsB
     private static final String OBJECT_ID_MANUFACTURER = "objectIDManufacturer";
     private static final String VIEW = "view";
     private static final String DEPTH = "depth";
+    private static final String ASPECT = "aspect";
 
     @Test
     public void getPartsTreeByObjectId_maintainedView_success() throws Exception {
@@ -135,6 +136,29 @@ public class GetPartsTreeByObjectIdIntegrationTests extends PrsIntegrationTestsB
                         .pathParam(OBJECT_ID_MANUFACTURER, PART_OBJECT_ID)
                         .queryParam(VIEW, AS_MAINTAINED)
                         .queryParam(DEPTH, 1)
+                .when()
+                        .get(PATH)
+                .then()
+                        .assertThat()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract().asString();
+
+        assertThatJson(response)
+                .when(IGNORING_ARRAY_ORDER)
+                .isEqualTo(json(expected));
+    }
+
+    @Test
+    public void getPartsTreeByObjectId_CEAspect_success() throws Exception {
+        var objectMapper = new ObjectMapper();
+        var expected = objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("sample_part_with_aspect_response.json"), PartRelationshipsWithInfos.class);
+
+        var response =
+                given()
+                        .pathParam(ONE_ID_MANUFACTURER, PART_ONE_ID)
+                        .pathParam(OBJECT_ID_MANUFACTURER, PART_OBJECT_ID)
+                        .queryParam(VIEW, AS_MAINTAINED)
+                        .queryParam(ASPECT, "CE")
                 .when()
                         .get(PATH)
                 .then()
--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/test/GetPartsTreeByVinIntegrationTests.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/test/GetPartsTreeByVinIntegrationTests.java
@@ -28,6 +28,7 @@ public class GetPartsTreeByVinIntegrationTests extends PrsIntegrationTestsBase {
     private static final String VIN = "vin";
     private static final String VIEW = "view";
     private static final String DEPTH = "depth";
+    private static final String ASPECT = "aspect";
 
     @Test
     public void getPartsTreeByVin_maintainedView_success() throws Exception {
@@ -109,6 +110,28 @@ public class GetPartsTreeByVinIntegrationTests extends PrsIntegrationTestsBase {
                         .pathParam(VIN, SAMPLE_VIN)
                         .queryParam(VIEW, AS_MAINTAINED)
                         .queryParam(DEPTH, 1)
+                .when()
+                        .get(PATH)
+                .then()
+                        .assertThat()
+                        .statusCode(HttpStatus.OK.value())
+                        .extract().asString();
+
+        assertThatJson(response)
+                .when(IGNORING_ARRAY_ORDER)
+                .isEqualTo(json(expected));
+    }
+
+    @Test
+    public void getPartsTreeByVin_CEAspect_success() throws Exception {
+        var objectMapper = new ObjectMapper();
+        var expected = objectMapper.readValue(getClass().getClassLoader().getResourceAsStream("sample_vin_with_aspect_response.json"), PartRelationshipsWithInfos.class);
+
+        var response =
+                given()
+                        .pathParam(VIN, SAMPLE_VIN)
+                        .queryParam(VIEW, AS_MAINTAINED)
+                        .queryParam(ASPECT, "CE")
                 .when()
                         .get(PATH)
                 .then()
