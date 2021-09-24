aece7f92831c5ebe0fa8feb0be4cc23ce8e779a6
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-09-24 16:00:15 +0200

    split

--- coreservices/partsrelationshipservice/broker-proxy/src/test/java/net/catenax/brokerproxy/services/BrokerProxyServiceTest.java
+++ coreservices/partsrelationshipservice/broker-proxy/src/test/java/net/catenax/brokerproxy/services/BrokerProxyServiceTest.java
@@ -5,37 +5,34 @@ import com.catenax.partsrelationshipservice.dtos.messaging.PartRelationshipUpdat
 import com.github.javafaker.Faker;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-import net.catenax.brokerproxy.configuration.BrokerProxyConfiguration;
 import net.catenax.brokerproxy.exceptions.MessageProducerFailedException;
 import net.catenax.prs.testing.DtoMother;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.kafka.core.KafkaOperations;
-import org.springframework.scheduling.annotation.AsyncResult;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BrokerProxyServiceTest {
 
     @Mock
-    KafkaOperations<String, Object> kafka;
+    MessageProducerService producerService;
 
     @Spy
     MeterRegistry registry = new SimpleMeterRegistry();
-
-    @Spy
-    BrokerProxyConfiguration configuration = new BrokerProxyConfiguration();
 
     @Captor
     ArgumentCaptor<PartRelationshipUpdateListMessage> messageCaptor;
@@ -51,55 +48,41 @@ class BrokerProxyServiceTest {
     void setUp()
     {
         sut.initialize();
-        when(kafka.send(any(), any())).thenReturn(AsyncResult.forValue(null));
-        configuration.setKafkaTopic(faker.lorem().word());
     }
 
     @Test
-    void uploadPartRelationshipUpdateList_sendsMessageToBroker() {
-        // Arrange
-        var topic = configuration.getKafkaTopic();
-
+    void sendPartRelationshipUpdateList_sendsMessageToBroker() {
         // Act
-        sut.uploadPartRelationshipUpdateList(message);
+        sut.send(message);
 
         // Assert
-        verify(kafka).send(eq(topic), argThat(this::isExpectedBrokerMessage));
+        verify(producerService).send(argThat(this::isExpectedBrokerMessage));
     }
 
     @Test
-    void uploadPartRelationshipUpdateList_generatesDistinctUUIDs() {
+    void sendPartRelationshipUpdateList_generatesDistinctUUIDs() {
         // Arrange
         var nTimes = faker.number().numberBetween(2, 10);
 
         // Act
-        IntStream.range(0, nTimes).forEach(i -> sut.uploadPartRelationshipUpdateList(message));
+        IntStream.range(0, nTimes).forEach(i -> sut.send(message));
 
         // Assert
-        verify(kafka, times(nTimes)).send(anyString(), messageCaptor.capture());
+        verify(producerService, times(nTimes)).send(messageCaptor.capture());
 
         assertThat(messageCaptor.getAllValues().stream().map(v -> v.getPartRelationshipUpdateListId()))
                 .doesNotHaveDuplicates();
     }
 
     @Test
-    void uploadPartRelationshipUpdateList_onKafkaFailure_Throws() {
+    void sendPartRelationshipUpdateList_onProducerException_Throws() {
         // Arrange
-        verifyExceptionThrownOnFailure(new ExecutionException(new IOException()));
-    }
-
-    @Test
-    void uploadPartRelationshipUpdateList_onInterrupted_Throws() {
-        // Arrange
-        verifyExceptionThrownOnFailure(new InterruptedException());
-    }
-
-    private void verifyExceptionThrownOnFailure(Throwable exception) {
-        when(kafka.send(any(), any())).thenReturn(AsyncResult.forExecutionException(exception));
+        doThrow(new MessageProducerFailedException(new InterruptedException()))
+                .when(producerService).send(any());
 
         // Act
         assertThatExceptionOfType(MessageProducerFailedException.class).isThrownBy(() ->
-                sut.uploadPartRelationshipUpdateList(message));
+                sut.send(message));
     }
 
     private boolean isExpectedBrokerMessage(PartRelationshipUpdateListMessage m) {
