eafb6f9ea117e917ccd6f486f201700d60b1edee
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-25 11:17:50 +0200

    https://jira.catena-x.net/browse/CATART0PO6-5 remove certmanager validation for ingress resource groups

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -170,9 +170,9 @@ resource "azurerm_public_ip" "portal_ip" {
 resource "kubernetes_namespace" "ingress_service_namespace" {
   metadata {
     name = "ingress-service"
-    labels = {
-      "cert-manager.io/disable-validation" = "true"
-    }
+    #labels = {
+    #  "cert-manager.io/disable-validation" = "true"
+    #}
   }
 }
 
@@ -221,9 +221,9 @@ resource "helm_release" "nginx_ingress" {
 resource "kubernetes_namespace" "ingress_portal_namespace" {
   metadata {
     name = "ingress-portal"
-    labels = {
-      "cert-manager.io/disable-validation" = "true"
-    } 
+    #labels = {
+    #  "cert-manager.io/disable-validation" = "true"
+    #} 
   }
 }
 
