10bda671c316ef4edf1aef7278e2b395000af3e6
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2021-09-28 14:28:59 +0200

    https://jira.catena-x.net/browse/CATART0PO4-139 Try to circumvent provider reconfiguration bug.

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -1,18 +1,54 @@
+variable "aks_host" {
+  description = "Kubernetes Host. Use module.aks_services.kube_admin_config if not set."
+  type = string
+  default = ""
+}
+
+variable "aks_user" {
+  description = "Kubernetes User. Use module.aks_services.kube_admin_config if not set."
+  type = string
+  default = ""
+}
+
+variable "aks_password" {
+  description = "Kubernetes Password. Use module.aks_services.kube_admin_config if not set."
+  type = string
+  default = ""
+}
+
+variable "aks_client_key" {
+  description = "Kubernetes Client Key. Use module.aks_services.kube_admin_config if not set."
+  type = string
+  default = ""
+}
+
+variable "aks_client_certificate" {
+  description = "Kubernetes Client Certificate. Use module.aks_services.kube_admin_config if not set."
+  type = string
+  default = ""
+}
+
+variable "aks_cluster_certificate" {
+  description = "Kubernetes Cluster Certificate. Use module.aks_services.kube_admin_config if not set."
+  type = string
+  default = ""
+}
+
 provider "kubernetes" {
-  host                   = module.aks_services.kube_admin_config.0.host
-  username               = module.aks_services.kube_admin_config.0.username
-  password               = module.aks_services.kube_admin_config.0.password
-  client_key             = base64decode(module.aks_services.kube_admin_config.0.client_key)
-  client_certificate     = base64decode(module.aks_services.kube_admin_config.0.client_certificate)
-  cluster_ca_certificate = base64decode(module.aks_services.kube_admin_config.0.cluster_ca_certificate)
+  host                   = (aks_host != "(known after apply)" && aks_host != "") ? aks_host : module.aks_services.kube_admin_config.0.host
+  username               = (aks_user != "(known after apply)" && aks_user != "") ? aks_user : module.aks_services.kube_admin_config.0.username
+  password               = (aks_password != "(known after apply)" && aks_password != "") ? aks_password : module.aks_services.kube_admin_config.0.password 
+  client_key             = base64decode((aks_client_key != "(known after apply)" && aks_client_key != "") ? aks_client_key : module.aks_services.kube_admin_config.0.client_key)
+  client_certificate     = base64decode((aks_client_certificate != "(known after apply)" && aks_client_certificate != "") ? aks_client_certificate : module.aks_services.kube_admin_config.0.client_certificate)
+  cluster_ca_certificate = bavar-filese64decode((aks_cluster_certificate != "(known after apply)" && aks_cluster_certificate != "") ? aks_cluster_certificate : module.aks_services.kube_admin_config.0.cluster_ca_certificate)
 }
 
 provider "helm" {
     debug = true
     kubernetes {
-        host     = module.aks_services.kube_admin_config.0.host
-        client_key             = base64decode(module.aks_services.kube_admin_config.0.client_key)
-        client_certificate     = base64decode(module.aks_services.kube_admin_config.0.client_certificate)
-        cluster_ca_certificate = base64decode(module.aks_services.kube_admin_config.0.cluster_ca_certificate)
+      host                   = (aks_host != "(known after apply)" && aks_host != "") ? aks_host : module.aks_services.kube_admin_config.0.host
+      client_key             = base64decode((aks_client_key != "(known after apply)" && aks_client_key != "") ? aks_client_key : module.aks_services.kube_admin_config.0.client_key)
+      client_certificate     = base64decode((aks_client_certificate != "(known after apply)" && aks_client_certificate != "") ? aks_client_certificate : module.aks_services.kube_admin_config.0.client_certificate)
+      cluster_ca_certificate = bavar-filese64decode((aks_cluster_certificate != "(known after apply)" && aks_cluster_certificate != "") ? aks_cluster_certificate : module.aks_services.kube_admin_config.0.cluster_ca_certificate)
     }  
 }
