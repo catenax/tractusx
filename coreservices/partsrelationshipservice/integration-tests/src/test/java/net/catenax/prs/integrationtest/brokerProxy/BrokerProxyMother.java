2a8b19cbfdddad9a1cdca5950299533ab27fdfb0
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2021-10-04 01:28:11 +0200

    Updating test config & adding an integration test setup for broker api

--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/integrationtest/brokerProxy/BrokerProxyMother.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/integrationtest/brokerProxy/BrokerProxyMother.java
@@ -7,28 +7,25 @@
 // See the LICENSE file(s) distributed with this work for
 // additional information regarding license terms.
 //
-package net.catenax.brokerproxy;
+package net.catenax.prs.integrationtest.brokerProxy;
 
-import net.catenax.brokerproxy.requests.PartAspectUpdateRequest;
 import com.catenax.partsrelationshipservice.dtos.PartAttributeName;
-import net.catenax.brokerproxy.requests.PartAttributeUpdateRequest;
-import com.catenax.partsrelationshipservice.dtos.PartLifecycleStage;
-import net.catenax.brokerproxy.requests.PartRelationshipUpdate;
-import net.catenax.brokerproxy.requests.PartRelationshipUpdateRequest;
 import com.github.javafaker.Faker;
+import net.catenax.brokerproxy.requests.PartAspectUpdateRequest;
+import net.catenax.brokerproxy.requests.PartAttributeUpdateRequest;
 import net.catenax.prs.testing.DtoMother;
 
 import static java.util.Collections.singletonList;
 import static java.util.concurrent.TimeUnit.DAYS;
 
 /**
- * Object Mother to generate fake DTO data for testing.
+ * Object Mother to generate data for integration tests.
  *
  * @see <a href="https://martinfowler.com/bliki/ObjectMother.html">
  * https://martinfowler.com/bliki/ObjectMother.html</a>
  */
-@SuppressWarnings("checkstyle:MagicNumber")
-public class BrokerProxyDtoMother {
+public class BrokerProxyMother {
+
     /**
      * JavaFaker instance used to generate random data.
      */
@@ -37,31 +34,6 @@ public class BrokerProxyDtoMother {
      * Object Mother to generate core DTO data for testing.
      */
     private final transient DtoMother generate = new DtoMother();
-
-    /**
-     * Generate a {@link PartRelationshipUpdateRequest} containing random data.
-     *
-     * @return never returns {@literal null}.
-     */
-    public PartRelationshipUpdateRequest partRelationshipUpdateList() {
-        return PartRelationshipUpdateRequest.builder()
-                .withRelationships(singletonList(partRelationshipUpdate()))
-                .build();
-    }
-
-    /**
-     * Generate a {@link PartRelationshipUpdate} containing random data.
-     *
-     * @return never returns {@literal null}.
-     */
-    private PartRelationshipUpdate partRelationshipUpdate() {
-        return PartRelationshipUpdate.builder()
-                .withRelationship(generate.partRelationship())
-                .withRemove(false)
-                .withStage(faker.options().option(PartLifecycleStage.class))
-                .withEffectTime(faker.date().past(100, DAYS).toInstant())
-                .build();
-    }
 
     /**
      * Generate a {@link PartAspectUpdateRequest} containing random data.
