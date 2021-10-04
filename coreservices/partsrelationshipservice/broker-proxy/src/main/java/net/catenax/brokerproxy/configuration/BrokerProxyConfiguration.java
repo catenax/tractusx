1d09580591638e9a4b90e7e1d4e0115b19aa80ab
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2021-10-04 11:43:06 +0200

    SuppressFBWarnings for configuration field and excluding getter from coverage report

--- coreservices/partsrelationshipservice/broker-proxy/src/main/java/net/catenax/brokerproxy/configuration/BrokerProxyConfiguration.java
+++ coreservices/partsrelationshipservice/broker-proxy/src/main/java/net/catenax/brokerproxy/configuration/BrokerProxyConfiguration.java
@@ -9,7 +9,9 @@
 //
 package net.catenax.brokerproxy.configuration;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.Data;
+import net.catenax.brokerproxy.annotations.ExcludeFromCodeCoverageGeneratedReport;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -38,6 +40,8 @@ public class BrokerProxyConfiguration {
      * Gets kafka topic for parts relationships.
      * @return Parts relationship kafka topic name.
      */
+    @SuppressFBWarnings(value = "UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR", justification = "Map initialized via config.")
+    @ExcludeFromCodeCoverageGeneratedReport
     public String getPartsRelationshipTopic() {
         return kafkaTopics.get("relationships");
     }
@@ -46,6 +50,8 @@ public class BrokerProxyConfiguration {
      * Gets kafka topic for parts aspects.
      * @return Parts aspects kafka topic name.
      */
+    @SuppressFBWarnings(value = "UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR", justification = "Map initialized via config.")
+    @ExcludeFromCodeCoverageGeneratedReport
     public String getPartsAspectsTopic() {
         return kafkaTopics.get("aspects");
     }
@@ -54,6 +60,8 @@ public class BrokerProxyConfiguration {
      * Gets kafka topic for parts attributes.
      * @return Parts attributes kafka topic name.
      */
+    @SuppressFBWarnings(value = "UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR", justification = "Map initialized via config.")
+    @ExcludeFromCodeCoverageGeneratedReport
     public String getPartsAttributesTopic() {
         return kafkaTopics.get("attributes");
     }
