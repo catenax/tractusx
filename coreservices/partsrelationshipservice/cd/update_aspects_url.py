9c9fa6aa239f77fbdc0d0c0a05cbcecaf4ec6743
Ophelie Le Mentec <olementec@microsoft.com>
2021-10-12 14:23:44 +0200

    Made default aspect url an argument.

--- coreservices/partsrelationshipservice/cd/update_aspects_url.py
+++ coreservices/partsrelationshipservice/cd/update_aspects_url.py
@@ -2,14 +2,18 @@
 import json
 import sys
 
+# File containing the PartAspectUpdate json file that needs to be updated.
 part_aspect_updates_file=sys.argv[1]
+# File containing the mapping between oneIDManufacturer and their aspectUrl.
+# The format should be { "oneIDManufacturer1": "aspectUrl1", "oneIDManufacture21": "aspectUrl2"}
 aspect_urls_by_manufacturer_file=sys.argv[2]
+# File where the new PartAspectUpdate json will be written. It can be the same as the file used as an input.
 output_file=sys.argv[3]
+# Aspect URL that will be used if the oneIDManufacturer has no aspect url.
+default_aspect_url=sys.argv[4] # "http://tdmgeneratordev.germanywestcentral.azurecontainer.io:8080"
 
 with open(aspect_urls_by_manufacturer_file, 'r+') as f:
     aspect_urls_by_manufacturer_id = json.load(f)
-
-default_aspect_url = "http://tdmgeneratordev.germanywestcentral.azurecontainer.io:8080"
 
 with open(part_aspect_updates_file, 'r+') as f:
     part_aspect_updates = json.load(f)
