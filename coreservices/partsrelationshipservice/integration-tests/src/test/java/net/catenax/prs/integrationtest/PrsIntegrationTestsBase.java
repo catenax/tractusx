14500d8cea7d1e5b33e6da24a73be190f92a0c61
Izabela Kulakowska <ikulakowska@microsoft.com>
2021-10-13 16:36:04 +0200

    Changes after review

--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/integrationtest/PrsIntegrationTestsBase.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/integrationtest/PrsIntegrationTestsBase.java
@@ -159,7 +159,6 @@ public class PrsIntegrationTestsBase {
             props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
             props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
             props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-            props.put(ProducerConfig.ACKS_CONFIG, "all");
             props.put(ProducerConfig.RETRIES_CONFIG, 3);
             return props;
         }
