e2913e702e22dab0c87a66ddf6278d5683c1695e
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-10-13 10:00:42 +0200

    Revert "Update main.tf"
    This reverts commit 6c4640983e490424ff9b9f87082291ce93156d10.

--- coreservices/partsrelationshipservice/terraform/main.tf
+++ coreservices/partsrelationshipservice/terraform/main.tf
@@ -5,7 +5,7 @@
 module "prs_application_insights" {
   source = "./modules/application-insights"
 
-  name                 = "${var.prefix}-${var.environment}-prs-appi"
+  name                = "${var.prefix}-${var.environment}-prs-appi"
   resource_group_name = local.resource_group_name
   location            = local.location
 }
