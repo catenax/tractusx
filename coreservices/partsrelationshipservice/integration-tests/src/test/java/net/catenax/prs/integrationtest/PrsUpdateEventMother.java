aab08a6e62b2a2a98608f61af13fe8278145adf6
Izabela Kulakowska <ikulakowska@microsoft.com>
2021-10-14 12:58:05 +0200

    changes after review

--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/integrationtest/PrsUpdateEventMother.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/integrationtest/PrsUpdateEventMother.java
@@ -9,16 +9,11 @@
 //
 package net.catenax.prs.integrationtest;
 
-import com.catenax.partsrelationshipservice.dtos.PartAttribute;
 import com.catenax.partsrelationshipservice.dtos.PartLifecycleStage;
-import com.catenax.partsrelationshipservice.dtos.messaging.PartAspectUpdateEvent;
-import com.catenax.partsrelationshipservice.dtos.messaging.PartAttributeUpdateEvent;
 import com.catenax.partsrelationshipservice.dtos.messaging.PartRelationshipUpdateEvent;
-import com.github.javafaker.Faker;
 import net.catenax.prs.testing.DtoMother;
 
 import java.time.Instant;
-import java.util.List;
 
 /**
  * Object Mother to generate data for integration tests.
@@ -29,74 +24,10 @@ import java.util.List;
 public class PrsUpdateEventMother {
 
     /**
-     * JavaFaker instance used to generate random data.
-     */
-    private final transient Faker faker = new Faker();
-
-    /**
      * Dto mother object that generates test data.
      */
     private final DtoMother generate = new DtoMother();
 
-    /**
-     * Samples of the data for PRS Api.
-     */
-    private final PartsTreeApiResponseMother samples = new PartsTreeApiResponseMother();
-
-
-    /**
-     * Generate a {@link PartAttributeUpdateEvent} with sample data.
-     * @return see {@link PartAttributeUpdateEvent}.
-     */
-    public PartAttributeUpdateEvent sampleAttributeUpdateEvent() {
-        return PartAttributeUpdateEvent.builder()
-                .withEffectTime(Instant.now())
-                .withPart(samples.gearwheelpinPartId1())
-                .withName(PartAttribute.PART_TYPE_NAME)
-                .withValue(faker.lorem().word())
-                .build();
-    }
-
-    /**
-     * Generate a {@link PartAttributeUpdateEvent} with sample data.
-     * @param time effect time.
-     * @return see {@link PartAttributeUpdateEvent}.
-     */
-    public PartAttributeUpdateEvent sampleAttributeUpdateEvent(Instant time) {
-        return PartAttributeUpdateEvent.builder()
-                .withEffectTime(time)
-                .withPart(samples.gearwheelpinPartId1())
-                .withName(PartAttribute.PART_TYPE_NAME)
-                .withValue(faker.lorem().word())
-                .build();
-    }
-
-    /**
-     * Generate a {@link PartAttributeUpdateEvent} with sample data.
-     * @return see {@link PartAttributeUpdateEvent}.
-     */
-    public PartAspectUpdateEvent sampleAspectsUpdateEvent() {
-
-        return PartAspectUpdateEvent.builder()
-                .withEffectTime(Instant.now())
-                .withPart(samples.gearwheelpinPartId1())
-                .withAspects(List.of(generate.partAspect()))
-                .build();
-    }
-
-    /**
-     * Generate a {@link PartAspectUpdateEvent} with sample data.
-     * @param time effect time.
-     * @return see {@link PartAspectUpdateEvent}.
-     */
-    public PartAspectUpdateEvent sampleAspectsUpdateEvent(Instant time) {
-
-        return PartAspectUpdateEvent.builder()
-                .withEffectTime(time)
-                .withPart(samples.gearwheelpinPartId1())
-                .withAspects(List.of(generate.partAspect()))
-                .build();
-    }
 
     /**
      * Generate a {@link PartRelationshipUpdateEvent.RelationshipUpdate} with sample data.
@@ -117,11 +48,9 @@ public class PrsUpdateEventMother {
      * @return see {@link PartRelationshipUpdateEvent.RelationshipUpdate}.
      */
     protected PartRelationshipUpdateEvent.RelationshipUpdate sampleRelationshipUpdate(Instant time) {
-        return PartRelationshipUpdateEvent.RelationshipUpdate.builder()
+        return sampleRelationshipUpdate()
+                .toBuilder()
                 .withEffectTime(time)
-                .withRemove(false)
-                .withStage(PartLifecycleStage.BUILD)
-                .withRelationship(generate.partRelationship())
                 .build();
     }
 }
