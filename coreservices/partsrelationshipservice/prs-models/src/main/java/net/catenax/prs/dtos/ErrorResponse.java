6fe0c7560bf5b40f650ba4190eed89a5e2a233f2
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-10-22 08:08:28 +0200

    Update ErrorResponse.java

--- coreservices/partsrelationshipservice/prs-models/src/main/java/net/catenax/prs/dtos/ErrorResponse.java
+++ coreservices/partsrelationshipservice/prs-models/src/main/java/net/catenax/prs/dtos/ErrorResponse.java
@@ -1,12 +1,3 @@
-//
-// Copyright (c) 2021 Copyright Holder (Catena-X Consortium)
-//
-// See the AUTHORS file(s) distributed with this work for additional
-// information regarding authorship.
-//
-// See the LICENSE file(s) distributed with this work for
-// additional information regarding license terms.
-//
 package net.catenax.prs.dtos;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
