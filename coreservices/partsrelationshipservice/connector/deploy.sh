94e3371d164e1677730a8cd676b1c8cc2dae1384
Ophelie Le Mentec <olementec@microsoft.com>
2021-11-09 12:38:59 +0100

    Splitted in 2 helm chart.

--- coreservices/partsrelationshipservice/connector/deploy.sh
+++ coreservices/partsrelationshipservice/connector/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -euo pipefail
-
+set -euox pipefail
+TARGET=$1
+PARTITION=$2
 export DOCKER_BUILDKIT=1
 
 # set default values, if not provided
@@ -9,15 +10,12 @@ TAG=${TAG:-dev}
 
 az acr login -n $REGISTRY
 
-for TARGET in provider consumer; do
-  IMAGE=prs-connector-$TARGET
-  docker build ../ --target $TARGET -t $REGISTRY/$IMAGE:$TAG --build-arg BUILD_TARGET=$IMAGE --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD --target $IMAGE
-  docker push $REGISTRY/$IMAGE:$TAG
-done
+IMAGE=prs-connector-$TARGET
+echo $IMAGE
+docker build ../ --target $TARGET -t $REGISTRY/$IMAGE:$TAG --build-arg BUILD_TARGET=$IMAGE --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD --target $IMAGE
+docker push $REGISTRY/$IMAGE:$TAG
 
-helm delete prs-connector || true
-helm upgrade --install --wait prs-connector ../cd/helm/prs-connector \
-  --set provider.image.repository=$REGISTRY/prs-connector-provider \
-  --set provider.image.tag=$TAG \
-  --set consumer.image.repository=$REGISTRY/prs-connector-consumer \
-  --set consumer.image.tag=$TAG \
+helm delete prs-${TARGET} || true
+helm upgrade --install --wait prs-${TARGET} ../cd/helm/prs-${TARGET} \
+  --set image.repository=$REGISTRY/$IMAGE \
+  --set image.tag=$TAG
