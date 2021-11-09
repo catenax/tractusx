1254ca174589f6bfa2e9993f6026c71f16f5f33f
Ophelie Le Mentec <olementec@microsoft.com>
2021-11-09 11:04:29 +0100

    Changed path.

--- coreservices/partsrelationshipservice/connector/prs-connector-consumer/src/main/java/org/eclipse/dataspaceconnector/extensions/api/ConsumerApiController.java
+++ coreservices/partsrelationshipservice/connector/prs-connector-consumer/src/main/java/org/eclipse/dataspaceconnector/extensions/api/ConsumerApiController.java
@@ -24,7 +24,7 @@ import static java.lang.String.format;
 
 @Consumes({MediaType.APPLICATION_JSON})
 @Produces({MediaType.APPLICATION_JSON})
-@Path("/connector/consumer")
+@Path("/")
 public class ConsumerApiController {
 
     private final Monitor monitor;
