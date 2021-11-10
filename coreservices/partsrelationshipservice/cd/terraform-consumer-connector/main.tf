348d3aa907d4dbf5485d5a3755b469a5b27a492f
Ophelie Le Mentec <olementec@microsoft.com>
2021-11-10 09:34:57 +0100

    Moved connectors to consumer connector terraform.

---
+++ coreservices/partsrelationshipservice/cd/terraform-consumer-connector/main.tf
@@ -0,0 +1,61 @@
+# Create one consumer and one provider. This is a temporary set up.
+# Later on, providers will be deployed in terraform-dataspace-partition as we will have one provider per partition.
+# The consumer will have its own terraform and will use terraform-dataspace-partition output to be aware of all the
+# provider urls.
+
+resource "kubernetes_namespace" "prs-connectors" {
+  metadata {
+    name = "prs-connectors"
+  }
+}
+
+# Deploy the PRS Consumer with Helm
+resource "helm_release" "prs-consumer" {
+  name      = "prs-consumer"
+  chart     = "../helm/prs-consumer"
+  namespace = kubernetes_namespace.prs-connectors.metadata[0].name
+  timeout   = 300
+
+  set {
+    name  = "ingress.host"
+    value = var.consumer_ingress_host
+  }
+
+  set {
+    name  = "ingress.className"
+    value = var.ingress_class_name
+  }
+
+  set {
+    name  = "ingress.prefix"
+    value = "/prs-consumer"
+  }
+
+  set {
+    name  = "image.repository"
+    value = "${var.image_registry}/prs-connector-consumer"
+  }
+
+  set {
+    name  = "image.tag"
+    value = var.image_tag
+  }
+}
+
+# Deploy the PRS Provider with Helm
+resource "helm_release" "prs-provider" {
+  name      = "prs-provider"
+  chart     = "../helm/prs-provider"
+  namespace = kubernetes_namespace.prs-connectors.metadata[0].name
+  timeout   = 300
+
+  set {
+    name  = "image.repository"
+    value = "${var.image_registry}/prs-connector-provider"
+  }
+
+  set {
+    name  = "image.tag"
+    value = var.image_tag
+  }
+}
