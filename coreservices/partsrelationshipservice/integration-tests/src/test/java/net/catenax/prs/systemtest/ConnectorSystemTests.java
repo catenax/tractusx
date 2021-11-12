27025f14a01dd8130bd0630c26ac214a79a622c3
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-11-12 10:33:23 +0100

    formatting

--- coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/systemtest/ConnectorSystemTests.java
+++ coreservices/partsrelationshipservice/integration-tests/src/test/java/net/catenax/prs/systemtest/ConnectorSystemTests.java
@@ -74,9 +74,9 @@ public class ConnectorSystemTests {
                 given()
                         .contentType("application/json")
                         .body(params)
-                        .when()
+                .when()
                         .post("/api/file")
-                        .then()
+                .then()
                         .assertThat()
                         .statusCode(HttpStatus.OK.value())
                         .extract().asString();
