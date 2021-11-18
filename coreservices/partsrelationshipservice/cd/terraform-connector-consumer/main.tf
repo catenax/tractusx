0baed3bf445b1c9c567d1cd1230ebc267f231a10
Ophelie Le Mentec <olementec@microsoft.com>
2021-11-18 13:40:12 +0100

    Fixed fmt.

--- coreservices/partsrelationshipservice/cd/terraform-connector-consumer/main.tf
+++ coreservices/partsrelationshipservice/cd/terraform-connector-consumer/main.tf
@@ -95,9 +95,9 @@ resource "helm_release" "prs-connector-provider" {
 }
 
 module "connector_storage" {
-  source      = "./modules/connector_storage"
-  environment = var.environment
-  location    = local.location
-  prefix      = var.prefix
+  source              = "./modules/connector_storage"
+  environment         = var.environment
+  location            = local.location
+  prefix              = var.prefix
   resource_group_name = var.resource_group_name
 }
