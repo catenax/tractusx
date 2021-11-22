96024e8018eb41e8a4d011204749f1521b0ce6e3
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-11-22 08:01:05 +0100

    Update run-integration-test.sh

--- coreservices/partsrelationshipservice/connector/run-integration-test.sh
+++ coreservices/partsrelationshipservice/connector/run-integration-test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-
-if ! [ -f /tmp/cert.pfx ]; then
-  echo "ERROR: Missing file /tmp/cert.pfx (see README.md)"
-  exit 1
-fi
-
 cd ..
 export DOCKER_BUILDKIT=1
 docker-compose --profile connector build --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD
