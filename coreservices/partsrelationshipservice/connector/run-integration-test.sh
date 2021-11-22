dd5c62fd1b803d8847f128ba2dcd09029e1890b7
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-11-22 08:00:41 +0100

    Update run-integration-test.sh

--- coreservices/partsrelationshipservice/connector/run-integration-test.sh
+++ coreservices/partsrelationshipservice/connector/run-integration-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-
 cd ..
 export DOCKER_BUILDKIT=1
 docker-compose --profile connector build --build-arg PRS_EDC_PKG_USERNAME=$PRS_EDC_PKG_USERNAME --build-arg PRS_EDC_PKG_PASSWORD=$PRS_EDC_PKG_PASSWORD
