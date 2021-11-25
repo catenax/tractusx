4fbbf64288e3b39e7d34ff9e14c8ad629792754a
Ophelie Le Mentec <olementec@microsoft.com>
2021-11-25 15:03:59 +0100

    Format.

--- coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsPerformanceTest.java
+++ coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsPerformanceTest.java
@@ -8,15 +8,13 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class PrsPerformanceTest extends Simulation {
 
-    private static final String prsURI = System.getProperty("PrsURI",
-            "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/bmw/mtpdc/prs");
+    private static final String prsURI = System.getProperty("PrsURI", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/bmw/mtpdc/prs");
     private static final String VEHICLE_ONEID = "CAXSWPFTJQEVZNZZ";
     private static final String VEHICLE_OBJECTID = "UVVZI9PKX5D37RFUB";
     private static final String VIEW = "AS_BUILT";
     private static final int DEPTH = 2;
 
-    private HttpProtocolBuilder httpProtocol = http
-            .baseUrl(prsURI);
+    private HttpProtocolBuilder httpProtocol = http.baseUrl(prsURI);
 
     final String pathParams = String.format("/api/v0.1/parts/%s/%s/partsTree?view=%s&aspect=CE&depth=%s", VEHICLE_ONEID, VEHICLE_OBJECTID, VIEW, DEPTH);
     private ScenarioBuilder scn = scenario("BasicSimulationJava")
