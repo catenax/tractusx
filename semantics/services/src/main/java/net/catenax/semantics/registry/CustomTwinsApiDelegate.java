a869e04ced39ddefd171b8be02ef1215ad355a90
Lukas Römer <lukas.roemer@bosch.io>
2021-11-25 10:21:01 +0100

    Extend get twins endpoint for pagination parameters

--- semantics/services/src/main/java/net/catenax/semantics/registry/CustomTwinsApiDelegate.java
+++ semantics/services/src/main/java/net/catenax/semantics/registry/CustomTwinsApiDelegate.java
@@ -63,8 +63,8 @@ public class CustomTwinsApiDelegate implements TwinsApiDelegate {
    }
 
    @Override
-   public ResponseEntity<DigitalTwinCollection> getTwinByQuery( final String key, final String value ) {
-      DigitalTwinCollection twinCollection = persistence.getTwins(key, value);
+   public ResponseEntity<DigitalTwinCollection> getTwinByQuery(Integer pageSize, Integer page, String key, String value) {
+      DigitalTwinCollection twinCollection = persistence.getTwins(key, value, pageSize, page);
 
       return new ResponseEntity<>(twinCollection, HttpStatus.OK );
    }
