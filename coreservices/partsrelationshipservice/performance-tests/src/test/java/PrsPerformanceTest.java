8821f99148618247f998eb1a7ef510b2d4e4c424
Ophelie Le Mentec <olementec@microsoft.com>
2021-11-25 15:44:34 +0100

    Used env instead of property.

--- coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsPerformanceTest.java
+++ coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsPerformanceTest.java
@@ -8,7 +8,7 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class PrsPerformanceTest extends Simulation {
 
-    private static final String prsURI = System.getProperty("PrsURI", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/bmw/mtpdc/prs");
+    private static final String prsURI = System.getenv().getOrDefault("PrsURI", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/bmw/mtpdc/prs");
     private static final String VEHICLE_ONEID = "CAXSWPFTJQEVZNZZ";
     private static final String VEHICLE_OBJECTID = "UVVZI9PKX5D37RFUB";
     private static final String VIEW = "AS_BUILT";
