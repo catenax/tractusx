0d0c575cd7de97deaba3e10b2ad2a2ccf94baeeb
Ophelie Le Mentec <olementec@microsoft.com>
2021-11-26 15:16:30 +0100

    Call connector instead of prs.

--- coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsPerformanceTest.java
+++ coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsPerformanceTest.java
@@ -6,25 +6,24 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class PrsPerformanceTest extends Simulation {
 
-    private static final String prsURI = System.getenv().getOrDefault("PrsURI", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/bmw/mtpdc/prs");
+    private static final String connectorUri = System.getenv().getOrDefault("ConnectorURI", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/prs-connector-consumer/api/v0.1");
     private static final String VEHICLE_ONEID = "CAXSWPFTJQEVZNZZ";
     private static final String VEHICLE_OBJECTID = "UVVZI9PKX5D37RFUB";
     private static final String VIEW = "AS_BUILT";
     private static final int DEPTH = 2;
 
-    private HttpProtocolBuilder httpProtocol = http.baseUrl(prsURI);
-
-    final String pathParams = String.format("/api/v0.1/parts/%s/%s/partsTree?view=%s&aspect=CE&depth=%s", VEHICLE_ONEID, VEHICLE_OBJECTID, VIEW, DEPTH);
+    private HttpProtocolBuilder httpProtocol = http.baseUrl(connectorUri)
+            .acceptHeader("*/*").contentTypeHeader("application/json");
     private ScenarioBuilder scn = scenario("Get parts tree for a part.")
             .repeat(3)
             .on(exec(
                     http("Trigger partsTree request")
-                            .get(pathParams)
+                            .post("/file")
+                            .body(RawFileBody("performance-tests/src/test/java/body.json"))
                             .check(status().is(200))
-            ).pause(1)
+            )/*.pause(1)
                     .doWhile("#{condition}")
-                    .on(exec(http("Get status").get(pathParams).check(status().is(200))))
-            );
+                    .on(exec(http("Get status").get(pathParams)))*/);
     {
         setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
     }
