247ee0a5a45a594bcdb7d677b08c0e86a501eb25
Alexandre Gattiker <algattik@users.noreply.github.com>
2021-11-29 15:45:07 +0100

    fix deps

--- coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsConnectorPerformanceTest.java
+++ coreservices/partsrelationshipservice/performance-tests/src/test/java/PrsConnectorPerformanceTest.java
@@ -1,9 +1,7 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
-import net.catenax.prs.requests.PartsTreeByObjectIdRequest;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -18,14 +16,12 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 public class PrsConnectorPerformanceTest extends Simulation {
 
     private static final String connectorUri = System.getenv().getOrDefault("ConnectorURI", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/prs-connector-consumer/api/v0.1");
-    // TODO: connectorAddress Should be removed after PR #307 gets merged.
-    private static final String providerAddress = System.getenv().getOrDefault("ProviderAddress", "https://catenaxdev001akssrv.germanywestcentral.cloudapp.azure.com/bmw/mtpdc/connector");
     private static final String VEHICLE_ONEID = "CAXSWPFTJQEVZNZZ";
     private static final String VEHICLE_OBJECTID = "UVVZI9PKX5D37RFUB";
     private static final String ASPECT_MATERIAL = "MATERIAL";
     private static final String VIEW = "AS_BUILT";
     private static final int DEPTH = 2;
-    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new Jdk8Module());
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private HttpProtocolBuilder httpProtocol = http.baseUrl(connectorUri)
             .acceptHeader("*/*").contentTypeHeader("application/json");
@@ -43,20 +39,18 @@ public class PrsConnectorPerformanceTest extends Simulation {
             )
                     // Call status endpoint every second, till it gives a 200 status code.
                     .exec(session -> session.set("status", -1))
-                    .doWhile(session -> session.getInt("status") != 200)
+                    .doWhileDuring(session -> session.getInt("status") != 200, Duration.ofSeconds(120))
                     .on(exec(http("Get status")
                             .get(session -> String.format("/datarequest/%s/state", session.getString("requestId")))
                             .check(status().saveAs("status")))
                             .pause(Duration.ofSeconds(1))));
 
     {
-        setUp(scenarioBuilder.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
+        setUp(scenarioBuilder.injectOpen(atOnceUsers(10))).protocols(httpProtocol);
     }
 
     private static String getSerializedPartsTreeRequest() {
         Map<String, Object> params = new HashMap<>();
-        // TODO: connectorAddress Should be removed after PR #307 gets merged.
-        params.put("connectorAddress", providerAddress);
         params.put("partsTreeRequest", PartsTreeByObjectIdRequest.builder()
                 .oneIDManufacturer(VEHICLE_ONEID)
                 .objectIDManufacturer(VEHICLE_OBJECTID)
@@ -66,7 +60,7 @@ public class PrsConnectorPerformanceTest extends Simulation {
                 .build());
 
         try {
-            return objectMapper.writeValueAsString(params);
+            return MAPPER.writeValueAsString(params);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             throw new RuntimeException("Exception serializing parts tree request", e);
