fc90e4d05f6db3a1a242841f7e32bd53ed6fb6a2
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2021-12-01 16:44:41 +0530

    Add UTs

--- coreservices/partsrelationshipservice/prs-models/src/test/java/net/catenax/prs/validators/PartRelationshipValidatorTest.java
+++ coreservices/partsrelationshipservice/prs-models/src/test/java/net/catenax/prs/validators/PartRelationshipValidatorTest.java
@@ -1,24 +1,30 @@
 package net.catenax.prs.validators;
 
-import net.catenax.prs.testing.BaseDtoMother;
-import net.catenax.prs.testing.DtoMother;
+import net.catenax.prs.dtos.PartRelationship;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.validation.ConstraintValidatorContext;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class PartRelationshipValidatorTest {
-
-    /**
-     * Base Object Mother to generate core DTO data for testing.
-     */
-    protected static final BaseDtoMother generateBaseDto = new BaseDtoMother();
-    /**
-     * Object Mother to generate core DTO data for testing.
-     */
-    protected static final DtoMother generateDto = new DtoMother();
 
     PartRelationshipValidator sut = new PartRelationshipValidator();
 
     @ParameterizedTest(name = "{index} {0}")
-    public void validate() {
+    @MethodSource("providePartRelationship")
+    public void validate(String testName, PartRelationship partRelationship, boolean expected) {
+        boolean actual = sut.isValid(partRelationship, mock(ConstraintValidatorContext.class));
+        assertThat(actual).isEqualTo(expected);
+    }
 
+    private static Stream<Arguments> providePartRelationship() {
+        return Stream.of(
+                Arguments.of("Null", null, true)
+        );
     }
 }
