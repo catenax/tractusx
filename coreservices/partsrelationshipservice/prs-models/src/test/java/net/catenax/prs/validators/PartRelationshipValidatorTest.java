573eaf2cd821495ceb3e059a8cb8ef8d1415c198
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2021-12-01 18:02:10 +0530

    Add UTs

--- coreservices/partsrelationshipservice/prs-models/src/test/java/net/catenax/prs/validators/PartRelationshipValidatorTest.java
+++ coreservices/partsrelationshipservice/prs-models/src/test/java/net/catenax/prs/validators/PartRelationshipValidatorTest.java
@@ -1,5 +1,7 @@
 package net.catenax.prs.validators;
 
+import com.github.javafaker.Faker;
+import net.catenax.prs.dtos.PartId;
 import net.catenax.prs.dtos.PartRelationship;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -13,6 +15,8 @@ import static org.mockito.Mockito.mock;
 
 public class PartRelationshipValidatorTest {
 
+    final static Faker faker = new Faker();
+
     PartRelationshipValidator sut = new PartRelationshipValidator();
 
     @ParameterizedTest(name = "{index} {0}")
@@ -23,8 +27,36 @@ public class PartRelationshipValidatorTest {
     }
 
     private static Stream<Arguments> providePartRelationship() {
+        var partId = partId();
+
         return Stream.of(
-                Arguments.of("Null", null, true)
+                Arguments.of("Null", null, true),
+                Arguments.of("Null Parent", PartRelationship.builder()
+                        .withParent(null).withChild(partId()).build(),
+                        true),
+                Arguments.of("Null Child", PartRelationship.builder()
+                                .withParent(partId()).withChild(null).build(),
+                        true),
+                Arguments.of("Parent and child with same part identifier", PartRelationship.builder()
+                                .withParent(partId).withChild(partId).build(),
+                        false),
+                Arguments.of("Parent and child with different part identifier", PartRelationship.builder()
+                                .withParent(partId()).withChild(partId()).build(),
+                        true)
         );
     }
+
+    /**
+     * Generate {@link PartId} with random values.
+     *
+     * @return a {@link PartId} with random identifiers.
+     */
+    private static PartId partId() {
+        return PartId.builder()
+                .withOneIDManufacturer(faker.company().name())
+                .withObjectIDManufacturer(faker.lorem().characters(10, 20))
+                .build();
+    }
+
+
 }
