6580ddb5ff094662a7f660b8c4729b375adf88a1
JaneMcBrain <contact@janemcbrain.com>
2021-12-08 15:17:10 +0100

    https://jira.catena-x.net/browse/A1ODT-99 add Material UI lib

---
+++ devops-tooling/dashboard/src/Theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#b3cb2d'
+    },
+    secondary: {
+      main: '#ffa600'
+    },
+  }
+});
+
+export default theme;
