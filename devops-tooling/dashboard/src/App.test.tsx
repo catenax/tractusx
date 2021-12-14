1539c67d9708ee2dbc57f56c121bf530bcc8f844
JaneMcBrain <contact@janemcbrain.com>
2021-12-14 15:58:31 +0100

    add routing tests

--- devops-tooling/dashboard/src/App.test.tsx
+++ devops-tooling/dashboard/src/App.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-const renderWithRouter = (ui, {route = '/'} = {}) => {
-  window.history.pushState({}, 'Test page', route)
-
-  return render(ui, {wrapper: BrowserRouter})
-}
 
 test('renders Login by default', () => {
   render(<App />);
+  const linkElement = screen.getByTestId("login");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders still Login when redirect to dashboard', () => {
+  render(<App />);
+  window.history.pushState({}, 'Test page', '/dashboard')
   const linkElement = screen.getByTestId("login");
   expect(linkElement).toBeInTheDocument();
 });
