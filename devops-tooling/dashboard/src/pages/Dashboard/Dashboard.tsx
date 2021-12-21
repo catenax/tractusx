c406ace660f5da8ef908bed3f9a04a040841cebb
= <=>
2021-12-21 11:03:01 +0100

    CATX-A1-ODT-42
    added placeholder for filters

--- devops-tooling/dashboard/src/pages/Dashboard/Dashboard.tsx
+++ devops-tooling/dashboard/src/pages/Dashboard/Dashboard.tsx
@@ -6,6 +6,8 @@ import useAuth from '../../Auth/useAuth';
 import Node from '../../Types/Node';
 import { useEffect, useRef, useState } from 'react';
 import theme from '../../Theme';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
 
 export default function Dashboard() {
   const [size, setSize] = useState<any>({width: null, height: null});
@@ -34,8 +36,25 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <Grid container direction="column" className="dashboard" data-testid="dashboard" ref={ref} sx={{height: `calc(100% - ${theme.spacing(8)})`}}>
-      {size.height && <NetworkGraph nodes={nodesData} links={linksData} parentSize={size}></NetworkGraph>}
-    </Grid>
+    <>
+      <Grid container spacing={1}>
+        <Grid item xs={4}>
+          <TextField  label="Search Connector " variant="outlined" fullWidth  />
+        </Grid>
+
+        <Grid item xs={4}>
+          <TextField  label="Start Date" variant="outlined" fullWidth />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField  label="End Date" variant="outlined" fullWidth />
+        </Grid>
+        <Grid item xs={3}>
+          <Button variant="contained" color="primary" > Search </Button>
+        </Grid>
+      </Grid>
+      <Grid container direction="column" className="dashboard" data-testid="dashboard" ref={ref} sx={{height: `calc(100% - ${theme.spacing(8)})`}}>
+        {size.height && <NetworkGraph nodes={nodesData} links={linksData} parentSize={size}></NetworkGraph>}
+      </Grid>
+    </>
   )
 }
