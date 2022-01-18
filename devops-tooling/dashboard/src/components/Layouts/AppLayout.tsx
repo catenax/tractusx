6ac2a93d3cdb6cdc4495a1408d208d82a0f60078
JaneMcBrain <contact@janemcbrain.com>
2022-01-18 23:11:54 +0100

    change layout and add new navigation

--- devops-tooling/dashboard/src/components/Layouts/AppLayout.tsx
+++ devops-tooling/dashboard/src/components/Layouts/AppLayout.tsx
@@ -1,71 +1,31 @@
-import { Box, CssBaseline, Drawer, AppBar, Toolbar, List, Typography, Divider, ListItem, ListItemIcon, ListItemText, Grid, Button  } from "@mui/material";
+import { Box, Button, Link  } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import MailIcon from '@mui/icons-material/Mail';
 import useAuth from '../../Auth/useAuth';
 import theme from "../../Theme";
-const drawerWidth = 240;
+import Header from "../Header/Header";
 
 export default function AppLayout() {
-
+  const navi = ['Connector Landscape', 'Tools', 'Warnings', 'FAQ', 'Configuration']
   const auth = useAuth();
 
   const handleLogoutClick = () => {
     auth.signOut(()=>console.log("logging out"))
   }
 
-
   return (
     <>
-      <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-        <CssBaseline />
-        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-          <Toolbar>
-            <Grid container direction="row" justifyContent="space-between" alignItems="center">
-              <Typography variant="h6" noWrap component="div"  >
-                Catena-X
-              </Typography>
-              <Button onClick={handleLogoutClick} color="inherit">Logout</Button>
-            </Grid>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-            [`& .MuiPaper-root`]: { marginTop: `${theme.spacing(8)}` },
-          }}
-        >
-          <Box sx={{ overflow: 'auto' }}>
-            <List>
-              <ListItem button>
-                <ListItemIcon>
-                  <DashboardIcon />
-                </ListItemIcon>
-                <ListItemText>Dashboard</ListItemText>
-              </ListItem>
-              <ListItem button>
-                <ListItemIcon>
-                  <MailIcon />
-                </ListItemIcon>
-                <ListItemText>Inactive</ListItemText>
-              </ListItem>
-            </List>
-            <Divider />
-            <List>
-              <ListItem button>
-                <ListItemIcon>
-                  <InboxIcon />
-                </ListItemIcon>
-                <ListItemText>Inactive</ListItemText>
-              </ListItem>
-            </List>
-          </Box>
-        </Drawer>
-        <Box component="main" sx={{flexGrow: 1, p: 3, mt: `${theme.spacing(8)}`}}>
+      <Box sx={{ display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        padding: `${theme.spacing(4)} ${theme.spacing(8)}`}}>
+        <Button onClick={handleLogoutClick} color="inherit" sx={{position: 'absolute', right: theme.spacing(8)}}>Logout</Button>
+        <Header></Header>
+        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+          {navi.map(item =>
+            <Link href="#" variant="body1" color='black' underline="hover" sx={{ml: theme.spacing(4), mr: theme.spacing(4)}}>{item}</Link>
+          )}
+        </Box>
+        <Box component="main" sx={{flexGrow: 1, p: 3, mt: theme.spacing(8)}}>
           <Outlet />
         </Box>
       </Box>
