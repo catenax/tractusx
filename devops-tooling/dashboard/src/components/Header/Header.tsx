1389285a8d51ca87b4c6835500bb8eae88a19d6c
JaneMcBrain <contact@janemcbrain.com>
2022-01-21 10:05:49 +0100

    add navigation component

--- devops-tooling/dashboard/src/components/Header/Header.tsx
+++ devops-tooling/dashboard/src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header(){
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', mb: theme.spacing(4)}}>
       <Typography sx={{mb: theme.spacing(1)}} variant='subtitle1' component='h1'>Catena-X</Typography>
-      <Typography component='h1'>Connector Dashboard</Typography>
+      <Typography component='h2'>Connector Dashboard</Typography>
     </Box>
   )
 }
--- devops-tooling/dashboard/src/components/Layouts/AppLayout.tsx
+++ devops-tooling/dashboard/src/components/Layouts/AppLayout.tsx
@@ -1,11 +1,11 @@
-import { Box, Button, Link  } from "@mui/material";
+import { Box, Button  } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import useAuth from '../../Auth/useAuth';
 import theme from "../../Theme";
 import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
 
 export default function AppLayout() {
-  const navi = ['Connector Landscape', 'Tools', 'Warnings', 'FAQ', 'Configuration']
   const auth = useAuth();
 
   const handleLogoutClick = () => {
@@ -20,11 +20,7 @@ export default function AppLayout() {
         padding: `${theme.spacing(4)} ${theme.spacing(8)}`}}>
         <Button onClick={handleLogoutClick} color="inherit" sx={{position: 'absolute', right: theme.spacing(8)}}>Logout</Button>
         <Header></Header>
-        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-          {navi.map(item =>
-            <Link href="#" variant="body1" color='black' underline="hover" sx={{ml: theme.spacing(4), mr: theme.spacing(4)}}>{item}</Link>
-          )}
-        </Box>
+        {auth.isAdmin() && <Navigation></Navigation>}
         <Box component="main" sx={{flexGrow: 1, mt: theme.spacing(4)}}>
           <Outlet />
         </Box>
