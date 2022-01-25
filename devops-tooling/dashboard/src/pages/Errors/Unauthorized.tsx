a04d86b68ad00b56c7c90385ca095a3ec5c68fa9
dhiraj-wakchaure <93985179+dhiraj-wakchaure@users.noreply.github.com>
2022-01-25 12:00:07 +0100

    lint fix

--- devops-tooling/dashboard/src/pages/Errors/Unauthorized.tsx
+++ devops-tooling/dashboard/src/pages/Errors/Unauthorized.tsx
@@ -10,32 +10,32 @@ export default function Unauthorized() {
 
 
   return (
-      <Box
-        sx={{
-          marginTop: theme.spacing(8),
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
+    <Box
+      sx={{
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
 
-        <ErrorOutlineOutlinedIcon sx={{fontSize:100}} />
+      <ErrorOutlineOutlinedIcon sx={{fontSize:100}} />
 
-        <Typography component="h2" variant="h2">
-            Not Allowed
-        </Typography>
+      <Typography component="h2" variant="h2">
+          Not Allowed
+      </Typography>
 
-        <Typography component="h5" variant="h5">
-          <Link
-            component={RouterLink}
-            to="/dashboard"
-            variant="body1"
-            color={theme.palette.common.black}
-            underline="always"
-            sx={{ml: theme.spacing(4), mr: theme.spacing(4)}}>
-              Go to home
-          </Link>
-        </Typography>
-      </Box>
+      <Typography component="h5" variant="h5">
+        <Link
+          component={RouterLink}
+          to="/dashboard"
+          variant="body1"
+          color={theme.palette.common.black}
+          underline="always"
+          sx={{ml: theme.spacing(4), mr: theme.spacing(4)}}>
+            Go to home
+        </Link>
+      </Typography>
+    </Box>
   );
 }
