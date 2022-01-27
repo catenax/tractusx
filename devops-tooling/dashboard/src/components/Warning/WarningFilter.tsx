e919c21456362bacb107e6bcb5633532cac470b4
dhiraj-wakchaure <93985179+dhiraj-wakchaure@users.noreply.github.com>
2022-01-27 12:28:06 +0100

    CATX-A1-ODT-223
    warning page as per figma design

---
+++ devops-tooling/dashboard/src/components/Warning/WarningFilter.tsx
@@ -0,0 +1,64 @@
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+import Datepicker from "../Datepicker/Datepicker";
+import theme from "../../Theme";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from "react";
+
+export default function WarningFilter() {
+
+  const [category] = useState("ALL");
+
+  return (
+    <Grid container alignItems='center' spacing={1} sx={{mb:theme.spacing(8)}}  >
+      <Grid item xs={2}>
+        <Typography sx={{ fontWeight: 'bold' }}>
+            From
+        </Typography>
+        <Datepicker />
+      </Grid>
+      <Grid item xs={2}>
+        <Typography sx={{ fontWeight: 'bold' }}>
+            To
+        </Typography>
+        <Datepicker />
+      </Grid>
+      <Grid item xs={2}>
+        <Typography sx={{ fontWeight: 'bold' }}>
+            Category
+        </Typography>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          fullWidth
+          value={category}>
+          <MenuItem value={"ALL"}>ALL</MenuItem>
+          <MenuItem value={20}>Some value</MenuItem>
+          <MenuItem value={30}>Some other value</MenuItem>
+        </Select>
+      </Grid>
+      <Grid item xs={2}>
+        <Typography sx={{ fontWeight: 'bold' }}>
+            Severity
+        </Typography>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          fullWidth
+          value={category}>
+          <MenuItem value={"ALL"}>HIGH</MenuItem>
+          <MenuItem value={20}>Some value</MenuItem>
+          <MenuItem value={30}>Some other value</MenuItem>
+        </Select>
+      </Grid>
+      <Grid item xs={2}>
+        <Button variant="contained" color="error" startIcon={<DeleteIcon />}>
+            Delete All
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
