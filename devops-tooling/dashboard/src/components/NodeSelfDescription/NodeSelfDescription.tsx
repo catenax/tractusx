fc17a3baa5c84cf6359db4a271838281e7f85214
= <=>
2022-02-04 10:08:55 +0100

    fixed react destructure props

--- devops-tooling/dashboard/src/components/NodeSelfDescription/NodeSelfDescription.tsx
+++ devops-tooling/dashboard/src/components/NodeSelfDescription/NodeSelfDescription.tsx
@@ -5,7 +5,7 @@ import Close from '@mui/icons-material/Close';
 import theme from '../../Theme';
 import DescriptionList from '../DescriptionList/DescriptionList';
 
-export default function NodeSelfDescription(props) {
+export default function NodeSelfDescription({ onClose, item }) {
   return (
     <Grid
       item
@@ -17,7 +17,7 @@ export default function NodeSelfDescription(props) {
     >
       <Button
         color="secondary"
-        onClick={() => props.onClose(null)}
+        onClick={() => onClose(null)}
         sx={{
           alignSelf: 'end', cursor: 'pointer', position: 'absolute', right: theme.spacing(1), top: theme.spacing(1),
         }}
@@ -26,15 +26,15 @@ export default function NodeSelfDescription(props) {
       </Button>
       <Typography variant="subtitle1" component="h3">Connector Details</Typography>
       <Typography variant="h5" component="h4" sx={{ mb: theme.spacing(2) }}>
-        <Link href={props.item['@id']} target="_blank" color="black" underline="none">
-          {props.item['ids:title'][0]['@value']}
+        <Link href={item['@id']} target="_blank" color="black" underline="none">
+          {item['ids:title'][0]['@value']}
         </Link>
       </Typography>
-      <DescriptionList topic="Format" link={props.item['@context'].ids} />
-      <DescriptionList topic="Type" description={props.item['@type']} />
-      <DescriptionList topic="Maintainer" link={props.item['ids:maintainer']['@id']} />
-      <DescriptionList topic="Curator" link={props.item['ids:curator']['@id']} />
-      <DescriptionList topic="Version" description={props.item['ids:outboundModelVersion']} />
+      <DescriptionList topic="Format" link={item['@context'].ids} />
+      <DescriptionList topic="Type" description={item['@type']} />
+      <DescriptionList topic="Maintainer" link={item['ids:maintainer']['@id']} />
+      <DescriptionList topic="Curator" link={item['ids:curator']['@id']} />
+      <DescriptionList topic="Version" description={item['ids:outboundModelVersion']} />
     </Grid>
   );
 }
