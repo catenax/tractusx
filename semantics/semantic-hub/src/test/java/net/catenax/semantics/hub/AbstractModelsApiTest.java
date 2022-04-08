2035edd7c4742456d22266be9075ed26f7e524fe
Fethullah Misir <fethullah.misir@de.bosch.com>
2022-04-08 15:35:35 +0200

    Implement authorization

---
+++ semantics/semantic-hub/src/test/java/net/catenax/semantics/hub/AbstractModelsApiTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021-2022 Robert Bosch Manufacturing Solutions GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.catenax.semantics.hub;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.catenax.semantics.GeneralProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@EnableConfigurationProperties(GeneralProperties.class)
+public abstract class AbstractModelsApiTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @Autowired
+    protected ObjectMapper mapper;
+
+    @Autowired
+    protected JwtTokenFactory jwtTokenFactory;
+
+
+}
